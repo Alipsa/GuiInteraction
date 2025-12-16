c5b4944259170120541a22b937273d40d4226ac3
per <per@alipsa.se>
2025-12-16 22:22:36 +0100

    improve release script

--- release.sh
+++ release.sh
@@ -1,1 +1,23 @@
-./gradlew clean build publish closeAndReleaseStagingRepositories
+#!/usr/bin/env bash
+set -e
+source ~/.sdkman/bin/sdkman-init.sh
+source jdk21
+PROJECT=$(basename "$PWD")
+
+if grep "version '" build.gradle | grep -q 'SNAPSHOT'; then
+  echo "$PROJECT snapshot cannot be published to maven central"
+  exit 1
+fi
+publish() {
+  sub=$1
+  echo "Publishing $sub to maven central"
+  ./gradlew :sub:clean :sub:build :sub:release
+}
+publish 'gi-common'
+publish 'gi-console'
+publish 'gi-fx'
+publish 'gi-swing'
+
+echo "$PROJECT uploaded and released"
+echo "see https://central.sonatype.org/publish/release/ for more info"
+#browse https://oss.sonatype.org &
